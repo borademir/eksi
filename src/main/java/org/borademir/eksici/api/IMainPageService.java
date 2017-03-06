d5852f2a76fade16f15b087caede61a400e5fba1
Bora Demir <bora@jforce.com.tr>
2017-03-06 22:21:26 +0300

    service interface init

---
+++ src/main/java/org/borademir/eksici/api/IMainPageService.java
@@ -0,0 +1,11 @@
+package org.borademir.eksici.api;
+
+import java.util.List;
+
+import org.borademir.eksici.api.model.TopicModel;
+
+public interface IMainPageService {
+	
+	public List<TopicModel> popularTopics();
+
+}
---
+++ src/main/java/org/borademir/eksici/api/impl/MainPageServiceJsoupImpl.java
@@ -0,0 +1,17 @@
+package org.borademir.eksici.api.impl;
+
+import java.util.List;
+
+import org.borademir.eksici.api.IMainPageService;
+import org.borademir.eksici.api.model.TopicModel;
+
+public class MainPageServiceJsoupImpl implements IMainPageService {
+
+	@Override
+	public List<TopicModel> popularTopics() {
+		
+		
+		return null;
+	}
+
+}
