5e42a7f57764e8e16066069dc43c3909d6c9ce51
Bora Demir <bora@jforce.com.tr>
2017-03-10 14:19:32 +0300

    rest api spring mvc / json impl ( search topic with keywords&authors)

--- api-web/src/main/java/org/borademir/eksici/spring/controller/EkisiciRestApiController.java
+++ api-web/src/main/java/org/borademir/eksici/spring/controller/EkisiciRestApiController.java
@@ -1,7 +1,5 @@
 package org.borademir.eksici.spring.controller;
 
-import java.io.IOException;
-
 import org.borademir.eksici.api.EksiApiException;
 import org.borademir.eksici.api.EksiApiServiceFactory;
 import org.borademir.eksici.api.IEksiService;
@@ -11,34 +9,27 @@ import org.borademir.eksici.api.model.SearchCriteriaModel;
 import org.borademir.eksici.api.model.TopicModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/v1")
+@RequestMapping("/v1")
 public class EkisiciRestApiController {
 	
 	@GetMapping("/search")
 	public ResponseEntity<MainPageModel> getCustomer( 
 			@RequestParam(value="keyword", required=true) String pKeyword,
-			@RequestParam(value="author", required=true) String pAuthor
-			
-			) {
+			@RequestParam(value="author", required=true) String pAuthor) throws EksiApiException {
 		
 		IEksiService eksiciService = EksiApiServiceFactory.createService();
 		MainPageModel mainPage = new MainPageModel();
 		SearchCriteriaModel searchCriteriaModel = new SearchCriteriaModel();
-//		searchCriteriaModel.setKeywords("aykut"); 
-//		searchCriteriaModel.setAuthor("qlluq");
-		
 		searchCriteriaModel.setKeywords(pKeyword); 
 		searchCriteriaModel.setAuthor(pAuthor);
-		
-		
 		mainPage.setSearchCriteria(searchCriteriaModel);
-	//	log.debug(searchCriteriaModel);
-	//	log.debug("Search results:");
 		GenericPager<TopicModel> searchResults = null;
 		StringBuffer buffy = new StringBuffer();
 		try {
@@ -47,15 +38,19 @@ public class EkisiciRestApiController {
 					buffy.append(tm.getTopicText() + "(" + tm.getRelatedEntryCount() + ") - " + tm.getOriginalUrl());
 				}
 			}
-			
 			return new ResponseEntity<MainPageModel>(mainPage, HttpStatus.OK);
-		} catch (EksiApiException e) {
-			return new ResponseEntity<MainPageModel>(mainPage, HttpStatus.NOT_FOUND);
-		} catch (IOException e) {
-			return new ResponseEntity<MainPageModel>(mainPage, HttpStatus.NOT_FOUND);
-		}
-	
-		
+		} catch (Exception e) {
+			throw new EksiApiException(e.getMessage());
+		} 
 	}	
+	
+	
+	@ExceptionHandler(EksiApiException.class)
+	public ResponseEntity<EksiciRestErrorResponse> exceptionHandler(Exception ex) {
+		EksiciRestErrorResponse error = new EksiciRestErrorResponse();
+		error.setErrorCode(HttpStatus.PRECONDITION_FAILED.value());
+		error.setMessage(ex.getMessage());
+		return new ResponseEntity<EksiciRestErrorResponse>(error, HttpStatus.OK);
+	}
 
 }
---
+++ api-web/src/main/java/org/borademir/eksici/spring/controller/EksiciRestErrorResponse.java
@@ -0,0 +1,21 @@
+package org.borademir.eksici.spring.controller;
+
+public class EksiciRestErrorResponse {
+	
+	private int errorCode;
+	private String message;
+	
+	public int getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
