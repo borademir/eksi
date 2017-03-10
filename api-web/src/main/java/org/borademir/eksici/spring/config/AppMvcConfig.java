46cc4cecde9ac53dd4c4c0a045a75b83540e7591
Bora Demir <bora@jforce.com.tr>
2017-03-10 15:21:52 +0300

    jackson mapper , ignore null fields

---
+++ api-web/src/main/java/org/borademir/eksici/spring/config/AppMvcConfig.java
@@ -0,0 +1,26 @@
+package org.borademir.eksici.spring.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+class AppMvcConfig extends WebMvcConfigurationSupport {
+	
+    @SuppressWarnings("rawtypes")
+	@Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for(HttpMessageConverter converter: converters) {
+            if(converter instanceof MappingJackson2HttpMessageConverter) {
+                ObjectMapper mapper = ((MappingJackson2HttpMessageConverter)converter).getObjectMapper();
+                mapper.setSerializationInclusion(Include.NON_NULL);
+            }
+        }
+    }
+}
